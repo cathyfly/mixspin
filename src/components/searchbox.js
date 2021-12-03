ee7a3552705182524762199342d77152b1f568ed
“[firstname <cathyflynnmedia@gmail.com>
2021-12-03 00:36:59 +0000

    add commands to voice search

---
+++ src/components/searchbox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from "react-bootstrap/Button";
+
+export const searchbox = () => {
+    return (
+        <div className="Button" id="search">
+       <Button>Hello</Button>
+       </div>
+       
+
+
+    )
+}
+
+export default searchbox
